508b8d60589b2478583d8b966bc538093303c948
Fabian Wilkens <wilkens.fabian+github@gmail.com>
2020-01-18 10:28:47 +0100

    Init commit

---
+++ scripts/_common.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+#=================================================
+# COMMON VARIABLES
+#=================================================
+
+# dependencies used by the app
+#pkg_dependencies="ruby ruby-dev rails zlib1g-dev libsqlite3-dev default-libmysqlclient-dev libssl-dev libreadline-dev"
+pkg_dependencies="                     zlib1g-dev libsqlite3-dev default-libmysqlclient-dev libssl-dev libreadline-dev"
+
+#=================================================
+# PERSONAL HELPERS
+#=================================================
+
+# Execute a command as another user with login
+# (hence in user home dir, with prior loading of .profile, etc.)
+# usage: exec_login_as USER COMMAND [ARG ...]
+exec_login_as() {
+	local user=$1
+	shift 1
+	exec_as $user --login "$@"
+}
+# Execute a command as another user
+# usage: exec_as USER COMMAND [ARG ...]
+exec_as() {
+	local user=$1
+	shift 1
+	
+	if [[ $user = $(whoami) ]]; then
+		eval "$@"
+	else
+		sudo -u "$user" "$@"
+	fi
+}
+
+#================================================
+# EXPERIMENTAL HELPERS
+#=================================================
+
+#=================================================
+# FUTURE OFFICIAL HELPERS
+#=================================================
