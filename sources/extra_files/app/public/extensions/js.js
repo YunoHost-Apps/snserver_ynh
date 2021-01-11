872e7574afb268c5165d090440dd7a4160da8c78
Fabian Wilkens <wilkens.fabian+github@gmail.com>
2021-01-11 22:33:16 +0100

    Cleanup

--- sources/extra_files/app/public/extensions/js.js
+++ sources/extra_files/app/public/extensions/js.js
@@ -1,23 +1,23 @@
 loadJSON("extensions/index.json");
 function loadJSON(file){
-    fetch(file)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        appendData(data);
-    })
-    .catch(function (err) {
-        console.log('error: ' + err);
-    });
+  fetch(file)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    appendData(data);
+  })
+  .catch(function (err) {
+    console.log('error: ' + err);
+  });
 }
 function appendData(data) {
-    for (var i = 0; i < data.length; i++) {
-        var mainContainer = document.getElementById(data[i].extension + "sData");
-        var tr = document.createElement("tr");
-        tr.innerHTML = 
-            "<td id='name'>" + data[i].name + "</td>" +
-            "<td id='url'><a href=" + data[i].url + ">" + data[i].url + "</a></td>"
-        mainContainer.appendChild(tr);
-    }
+  for (var i = 0; i < data.length; i++) {
+    var mainContainer = document.getElementById(data[i].extension + "sData");
+    var tr = document.createElement("tr");
+    tr.innerHTML = 
+      "<td id='name'>" + data[i].name + "</td>" +
+      "<td id='url'><a href=" + data[i].url + ">" + data[i].url + "</a></td>"
+    mainContainer.appendChild(tr);
+  }
 }
