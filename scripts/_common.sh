bfeeaa4b6fbe4061ab6e418a9e11072dbdbaa4bc
Fabian Wilkens <wilkens.fabian+github@gmail.com>
2021-05-02 11:47:41 +0200

    Switch to ynh_exec_as

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -18,27 +18,6 @@ pkg_dependencies="\
 # PERSONAL HELPERS
 #=================================================
 
-# Execute a command as another user with login
-# (hence in user home dir, with prior loading of .profile, etc.)
-# usage: exec_login_as USER COMMAND [ARG ...]
-exec_login_as() {
-    local user=$1
-    shift 1
-    exec_as $user --login "$@"
-}
-# Execute a command as another user
-# usage: exec_as USER COMMAND [ARG ...]
-exec_as() {
-    local user=$1
-    shift 1
-
-    if [[ $user = $(whoami) ]]; then
-        eval "$@"
-    else
-        sudo -u "$user" "$@"
-    fi
-}
-
 # Check if service is ready
 is_service_ready() {
     for ((i = 0 ; i < 15 ; i++))
