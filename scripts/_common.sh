204fcb8ba306b2bba3f1a98edf69cd5b71e79b95
Fabian Wilkens <wilkens.fabian+github@gmail.com>
2021-07-08 11:50:04 +0200

    Update conf/,scripts/

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -3,20 +3,16 @@
 #=================================================
 # COMMON VARIABLES
 #=================================================
-#RUBY_VERSION="2.7.3"
+RUBY_VERSION="2.7.3"
 NODEJS_VERSION="15.11.0"
+
+syncing_server_js_version="1.35.1"
+auth_version="1.11.0"
+api_gateway_version="1.19.6"
+extensions_version="2021.03.15"
 
 # dependencies used by the app
 pkg_dependencies=""
-
-#pkg_dependencies="\
-#    zlib1g-dev \
-#    libsqlite3-dev \
-#    default-libmysqlclient-dev \
-#    libssl-dev \
-#    libreadline-dev \
-#    libjemalloc-dev \
-#	"
 
 #=================================================
 # PERSONAL HELPERS
