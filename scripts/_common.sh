65ab05e6043fa5c8d27cc9ab94e7fbf15c6f7ee3
Fabian Wilkens <wilkens.fabian+github@gmail.com>
2021-07-09 20:35:47 +0200

    Fix: Update to latest versions

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,10 +6,10 @@
 RUBY_VERSION="2.7.3"
 NODEJS_VERSION="15.11.0"
 
-syncing_server_js_version="1.35.1"
-auth_version="1.11.0"
-api_gateway_version="1.19.6"
-extensions_version="2021.03.15"
+syncing_server_js_version="1.35.2"
+auth_version="1.11.1"
+api_gateway_version="1.20.0"
+extensions_version="2021.07.06"
 
 # dependencies used by the app
 pkg_dependencies=""
