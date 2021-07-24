9a77d8ccebb00f60106d3633ccca8ea970f35de0
Fabian Wilkens <wilkens.fabian+github@gmail.com>
2021-07-24 19:23:21 +0200

    Update: auth to latest version

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -7,7 +7,7 @@ RUBY_VERSION="2.7.3"
 NODEJS_VERSION="15.11.0"
 
 syncing_server_js_version="1.36.1"
-auth_version="1.11.1"
+auth_version="1.13.2"
 api_gateway_version="1.20.0"
 extensions_version="2021.07.06"
 
