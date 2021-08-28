38c29886b06a50b6ff8671efc967fd9153fa4fd7
Fabian Wilkens <wilkens.fabian+github@gmail.com>
2021-08-28 15:26:52 +0200

    Update: syncing_server_js to latest version

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,7 +6,7 @@
 RUBY_VERSION="2.7.3"
 NODEJS_VERSION="15.11.0"
 
-syncing_server_js_version="1.36.1"
+syncing_server_js_version="1.38.2"
 auth_version="1.13.2"
 api_gateway_version="1.20.5"
 extensions_version="2021.07.06"
