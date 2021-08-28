e417b6f255ff865b0e7e271cd3bf6f78d4d86b4e
Fabian Wilkens <wilkens.fabian+github@gmail.com>
2021-08-28 15:45:30 +0200

    Update: api-gateway to latest version

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -8,7 +8,7 @@ NODEJS_VERSION="15.11.0"
 
 syncing_server_js_version="1.38.2"
 auth_version="1.13.2"
-api_gateway_version="1.20.5"
+api_gateway_version="1.24.0"
 extensions_version="2021.07.06"
 
 # dependencies used by the app
