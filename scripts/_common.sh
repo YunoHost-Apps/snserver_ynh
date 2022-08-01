74aee8b7330f7e926ebcbf6c9743f6d474ab3ae9
Fabian Wilkens <wilkens.fabian+github@gmail.com>
2022-08-01 21:46:14 +0200

    Update: Remove self-hosted extensions

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -9,7 +9,6 @@ NODEJS_VERSION="16"
 syncing_server_js_version="1.54.1"
 auth_version="1.46.2"
 api_gateway_version="1.40.1"
-extensions_version="2021.08.27"
 
 swap_needed=2048
 node_max_old_space_size=6144
