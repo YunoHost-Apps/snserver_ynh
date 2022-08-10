353c35f573cb46519994284d5e5e4c6b23dca8cd
Fabian Wilkens <wilkens.fabian+github@gmail.com>
2022-08-10 16:48:52 +0200

    Fix: cleanup

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,11 +5,6 @@
 #=================================================
 NODEJS_VERSION="16"
 
-api_gateway_version="1.40.1"
-auth_version="1.46.2"
-files_version="0"
-syncing_server_version="1.54.1"
-
 swap_needed=2048
 node_max_old_space_size=6144
 
