437d1c023bd27bc7f5444fbfc07b5e928f5928f5
Fabian Wilkens <wilkens.fabian+github@gmail.com>
2022-08-11 13:20:52 +0200

    Fix: restore, remove change_url scripts

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -15,7 +15,37 @@ pkg_dependencies=""
 # PERSONAL HELPERS
 #=================================================
 
+# Substitute/replace a string (or expression) by another in a file on a
+#
+# usage: ynh_replace_string_occurrence --occurrence=occurrence --match_string=match_string --replace_string=replace_string --target_file=target_file
+# | arg: -o, --occurrence=      - Replace the nth occurrence in the file
+# | arg: -m, --match_string=    - String to be searched and replaced in the file
+# | arg: -r, --replace_string=  - String that will replace matches
+# | arg: -f, --target_file=     - File in which the string will be replaced.
+#
+# As this helper is based on sed command, regular expressions and references to
+# sub-expressions can be used (see sed manual page for more information)
+#
+ynh_replace_string_occurrence() {
+    # Declare an array to define the options of this helper.
+    local legacy_args=omrf
+    local -A args_array=([o]=occurrence [m]=match_string= [r]=replace_string= [f]=target_file=)
+    local occurrence
+    local match_string
+    local replace_string
+    local target_file
+    # Manage arguments with getopts
+    ynh_handle_getopts_args "$@"
+    set +o xtrace # set +x
 
+    local delimit=@
+    # Escape the delimiter if it's in the string.
+    match_string=${match_string//${delimit}/"\\${delimit}"}
+    replace_string=${replace_string//${delimit}/"\\${delimit}"}
+
+    set -o xtrace # set -x
+    sed --in-place "s${delimit}${match_string}${delimit}${replace_string}${delimit}${occurrence}" "$target_file"
+}
 
 #================================================
 # EXPERIMENTAL HELPERS
